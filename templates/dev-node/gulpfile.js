2f1cd625c60128936614813aef90600b858cb5b9
gogamoga <gogamoga@gmail.com>
2014-09-09 03:28:01 +0300

    Load plugins

---
+++ templates/dev-node/gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp'),
+  concatenate = require('gulp-concat'),
+  jshint = require('gulp-jshint'),
+  notify = require('gulp-notify'),
+  uglify = require('gulp-uglify')
+  del = require('del');
