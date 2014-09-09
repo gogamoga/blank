b81645255125a54a39af08ecb696214b908ff793
gogamoga <gogamoga@gmail.com>
2014-09-10 02:24:33 +0300

    simplified version with mocha

--- templates/dev-node/gulpfile.js
+++ templates/dev-node/gulpfile.js
@@ -1,79 +1,113 @@
-/* Config */
-var config = require('./package.json').blankConfig.gulp,
-  source = config.source,
-  build = config.build,
-  dist = config.dist,
-  concat = config.concat,
-  scripts = config.scripts;
-
-var scriptsSource = source
-var scriptsBuild = build
-var scriptsDist = dist
-
-/* Init */
-
-var i, key, list, copyList = [ source + '/' + scripts + '/**/*.js' ]
-for (key in concat) {
-  list = concat[key] || [];
-  for (i=0; i<list.length; i++) {
-    copyList.push('!' + source + '/' + scripts + '/' + list[i]);
-  }
-}
-
 /* Plugins */
 var gulp = require('gulp'),
-  concatenate = require('gulp-concat'),
   jshint = require('gulp-jshint'),
-  notify = require('gulp-notify'),
   rename = require('gulp-rename'),
   uglify = require('gulp-uglify'),
+  mocha = require('gulp-mocha'),
   del = require('del'),
   runsequence = require('run-sequence');
 
-gulp.task('jshint', function() {
-  return gulp.src(source + '/' + scripts + '/**/*.js')
+/* Config */
+var config = require('./package.json')['gulp-config'] || {},
+  source = config.source || './source'
+  build = config.build || './build'
+  dist = config.dist || './dist'
+  test = config.test || './test'
+
+/* Clean */
+
+gulp.task('clean-build', function(cb) {
+  del([build], cb);
+});
+
+gulp.task('clean-dist', function(cb) {
+  del([dist], cb);
+});
+
+gulp.task('clean', function(cb) {
+  runsequence(['clean-build', 'clean-dist'], cb);
+});
+
+/* Copy */
+gulp.task('copy-build', function() {
+  return gulp.src([ source + '/**/*', '!' + source + '/**/*.js' ])
+    .pipe(gulp.dest(build));
+});
+
+gulp.task('copy-dist', function() {
+  return gulp.src([ build + '/**/*', '!' + build + '/**/*.js' ])
+    .pipe(gulp.dest(dist));
+});
+
+/* Concatenate */
+gulp.task('concat-example', function() {
+  return gulp.src([])
+    .pipe(concatenate(''))
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('concat-build', function(cb) {
+  /* runsequence(cb); */
+	cb();
+});
+
+gulp.task('concat-dist', function(cb) {
+  /* runsequence(cb); */
+	cb();
+});
+
+/* JSHint */
+
+gulp.task('jshint-source', function() {
+  return gulp.src(source + '/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('copy', function() {
-  return gulp.src(copyList).pipe(gulp.dest(build + '/' + scripts));
+gulp.task('jshint-test', function() {
+  return gulp.src(test + '/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'));
 });
 
-gulp.task('concatenate', function() {
-  var i, key, list, last;
-  for (key in concat) {
-    list = concat[key] || [];
-    if(typeof list == 'string') list = [ list ];
-    for(i=0; i<list.length; i++) {
-      list[i] = source + '/' + scripts + '/' + list[i];
-    }
-    last = gulp.src(list)
-      .pipe(concatenate(key))
-      .pipe(gulp.dest(build + '/' + scripts));
-    }
-  return last;
+gulp.task('jshint', function(cb) {
+  runsequence(['jshint-source', 'jshint-test'], cb);
 });
 
-gulp.task('uglify', function() {
-  return gulp.src(dist + '/' + scripts + '/**/*.js')
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest(dist + '/' + scripts));
-});
-
-gulp.task('dist', function(cb) {
-  runsequence('clean', 'build', 'uglify', cb);
-});
-
-gulp.task('clean', function(cb) {
-  del([dist + '/' + scripts], cb);
+/* Build */
+gulp.task('build-source', function() {
+  return gulp.src(source + '/**/*.js')
+    .pipe(gulp.dest(build));
 });
 
 gulp.task('build', function(cb) {
-   runsequence('copy', 'concatenate', cb);
+  runsequence(['build-source', 'copy-build'], 'concat-build', cb);
 });
 
+/* Dist */
+gulp.task('dist-build', function() {
+  return gulp.src(build + '/**/*.js')
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('dist', function(cb) {
+  runsequence(['dist-build', 'copy-dist'], 'concat-dist', cb);
+});
+
+/* Test */
+gulp.task('mocha', function() {
+  return gulp.src([test + '/**/*.js'], { read: false })
+    .pipe(mocha({ reporter: 'spec', globals: { should: require('should') } }));
+});
+
+gulp.task('test', function(cb) {
+  runsequence('mocha', cb);
+});
+
+/* Default */
+
 gulp.task('default', function(cb) {
-  runsequence('build', cb);
+  runsequence('build', 'dist', cb);
 });
